e20addd360f318a4d9f78744d9e2ff7fa5e437db
Brayan Frank <brayanfrank.sm@gmail.com>
2022-03-21 20:33:12 -0300

    a bad responsivite

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -27,8 +27,8 @@ export default () => {
         <div className="app-left">
           <div className="app-header">
             <h1>Receitas</h1>
+            <Line backgroundColor="#093948" highlightedColor="#FAF86C"/>
           </div>
-          <Line backgroundColor="#093948" highlightedColor="#FAF86C"/>
           <div className="app-content">
             {
               dishesList.map((dish, i) => {
@@ -38,14 +38,12 @@ export default () => {
           </div>
         </div>
         <div className="app-right">
-          <div className="app-description">
             {
               detailsIsEnabled && selectedDish ?
                 <DishDetails selectedDish={selectedDish}/>
                 :
                 <></>
             }
-          </div>
         </div>
 
       </div>
